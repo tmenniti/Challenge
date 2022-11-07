3b4bf4d03ed772d24d0181b32eaa82001ad53cdf
Tomas Ariel Menniti <tmenniti@emeal.nttdata.com>
2022-11-07 18:25:45 -0300

    Initial commit

---
+++ app/src/main/java/com/intermedia/challenge/core/Constants.kt
@@ -0,0 +1,5 @@
+package com.company.moviesapp.core
+
+const val BASE_URL : String = "https://developer.marvel.com/"
+const val PUBLIC_KEY : String = "3a783b25c80e1c44875356dd363f272d"
+const val PRIVATE_KEY : String = "aa1141953df8c088f39a97de10008578e834580f"
---
+++ app/src/main/java/com/intermedia/challenge/ui/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.intermedia.challenge.ui.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.intermedia.challenge.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
